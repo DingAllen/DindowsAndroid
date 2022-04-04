231a0c72f54af92a1d84ab7c86616636c64a4b26
DingAllen <1121174884@qq.com>
2022-04-04 12:47:56 +0800

    基本实现底部Tab栏

--- app/src/main/java/online/dingod/dindowsandroid/demo/DingBottomDemoActivity.kt
+++ app/src/main/java/online/dingod/dindowsandroid/demo/DingBottomDemoActivity.kt
@@ -2,23 +2,66 @@ package online.dingod.dindowsandroid.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import online.dingod.dindowsandroid.R
+import online.dingod.dinglibrary.log.DingLog
 import online.dingod.dingui.tab.bottom.DingTabBottom
 import online.dingod.dingui.tab.bottom.DingTabBottomInfo
+import online.dingod.dingui.tab.bottom.DingTabBottomLayout
 
 class DingBottomDemoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ding_bottom_demo)
-        val tabBottom = findViewById<DingTabBottom>(R.id.tab_bottom)
+        initTabBottom()
+    }
+
+    private fun initTabBottom() {
+        val tabBottomLayout = findViewById<DingTabBottomLayout>(R.id.ding_tab_layout)
+        tabBottomLayout.setTabAlpha(0.85f)
+        val bottomInfoList: MutableList<DingTabBottomInfo<*>> = ArrayList()
         val homeInfo = DingTabBottomInfo(
             "首页",
             "font/iconfont.ttf",
             getString(R.string.if_home),
             null,
             "#ff656667",
-            "ffd44949"
+            "#ffd44949"
         )
-        tabBottom.setDingTabInfo(homeInfo)
+        val CollectionInfo = DingTabBottomInfo(
+            "收藏",
+            "font/iconfont.ttf",
+            getString(R.string.if_collection),
+            null,
+            "#ff656667",
+            "#ffd44949"
+        )
+        val ChatInfo = DingTabBottomInfo(
+            "聊天",
+            "font/iconfont.ttf",
+            getString(R.string.if_chat),
+            null,
+            "#ff656667",
+            "#ffd44949"
+        )
+        val ProfileInfo = DingTabBottomInfo(
+            "我的",
+            "font/iconfont.ttf",
+            getString(R.string.if_profile),
+            null,
+            "#ff656667",
+            "#ffd44949"
+        )
+        bottomInfoList.add(homeInfo)
+        bottomInfoList.add(CollectionInfo)
+        bottomInfoList.add(ChatInfo)
+        bottomInfoList.add(ProfileInfo)
+        tabBottomLayout.inflateInfo(bottomInfoList)
+        tabBottomLayout.addTabSelectedChangeListener{_,_, nextInfo ->
+            Toast.makeText(this@DingBottomDemoActivity, nextInfo.name, Toast.LENGTH_SHORT).show()
+
+        }
+        tabBottomLayout.defaultSelected(homeInfo)
     }
 }
--- dingui/src/main/java/online/dingod/dingui/tab/bottom/DingTabBottomLayout.java
+++ dingui/src/main/java/online/dingod/dingui/tab/bottom/DingTabBottomLayout.java
@@ -1,15 +1,23 @@
 package online.dingod.dingui.tab.bottom;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import online.dingod.dinglibrary.util.DingDisplayUtil;
+import online.dingod.dingui.R;
 import online.dingod.dingui.tab.common.IDingTabLayout;
 
 public class DingTabBottomLayout extends FrameLayout implements IDingTabLayout<DingTabBottom, DingTabBottomInfo<?>> {
@@ -21,6 +29,8 @@ public class DingTabBottomLayout extends FrameLayout implements IDingTabLayout<D
     private float bottomLineHeight = 0.5f;
     private String bottomLineColor = "#dfe0e1";
     private List<DingTabBottomInfo<?>> infoList;
+
+    private static final String TAG_TAB_BOTTOM = "TAG_TAB_BOTTOM";
 
     public DingTabBottomLayout(@NonNull Context context) {
         super(context);
@@ -34,19 +44,46 @@ public class DingTabBottomLayout extends FrameLayout implements IDingTabLayout<D
         super(context, attrs, defStyleAttr);
     }
 
+    public void setTabAlpha(float alpha) {
+        this.bottomAlpha = alpha;
+    }
+
+    public void setTabHeight(float tabHeight) {
+        this.tabBottomHeight = tabHeight;
+    }
+
+    public void setBottomLineHeight(float bottomLineHeight) {
+        this.bottomLineHeight = bottomLineHeight;
+    }
+
+    public void setBottomLineColor(String bottomLineColor) {
+        this.bottomLineColor = bottomLineColor;
+    }
+
     @Override
-    public DingTabBottom findTab(@NonNull DingTabBottomInfo<?> data) {
+    public DingTabBottom findTab(@NonNull DingTabBottomInfo<?> info) {
+
+        ViewGroup bottomFather = findViewWithTag(TAG_TAB_BOTTOM);
+        for (int i = 0; i < bottomFather.getChildCount(); i++) {
+            View child = bottomFather.getChildAt(i);
+            if (child instanceof DingTabBottom) {
+                DingTabBottom tab = (DingTabBottom) child;
+                if (tab.getTabBottomInfo() == info) {
+                    return tab;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public void addTabSelectedChangeListener(OnTabSelectedListener<DingTabBottomInfo<?>> listener) {
-
+        tabSelectedChangeListeners.add(listener);
     }
 
     @Override
     public void defaultSelected(@NonNull DingTabBottomInfo<?> defaultInfo) {
-
+        onSelected(defaultInfo);
     }
 
     @Override
@@ -62,9 +99,63 @@ public class DingTabBottomLayout extends FrameLayout implements IDingTabLayout<D
             removeViewAt(i);
         }
         selectedInfo = null;
+        addBackground();
+        Iterator<OnTabSelectedListener<DingTabBottomInfo<?>>> iterator = tabSelectedChangeListeners.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() instanceof DingTabBottom) {
+                iterator.remove();
+            }
+        }
+        FrameLayout bottomFather = new FrameLayout(getContext());
+        bottomFather.setTag(TAG_TAB_BOTTOM);
+        int width = DingDisplayUtil.getDisplayWidthInPx(getContext()) / infoList.size();
+        int height = DingDisplayUtil.dp2px(tabBottomHeight, getResources());
+        for (int i = 0; i < infoList.size(); i++) {
+            DingTabBottomInfo<?> info = infoList.get(i);
+            LayoutParams params = new LayoutParams(width, height);
+            params.gravity = Gravity.BOTTOM;
+            params.leftMargin = i * width;
+
+            DingTabBottom tabBottom = new DingTabBottom(getContext());
+            tabSelectedChangeListeners.add(tabBottom);
+            tabBottom.setDingTabInfo(info);
+            bottomFather.addView(tabBottom, params);
+            tabBottom.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onSelected(info);
+                }
+            });
+        }
+        LayoutParams flParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        flParams.gravity = Gravity.BOTTOM;
+        addBottomLine();
+        addView(bottomFather, flParams);
+    }
+
+    private void addBottomLine() {
+        View bottomLine = new View(getContext());
+        bottomLine.setBackgroundColor(Color.parseColor(bottomLineColor));
+
+        LayoutParams bottomLineParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, DingDisplayUtil.dp2px(bottomLineHeight, getResources()));
+        bottomLineParams.gravity = Gravity.BOTTOM;
+        bottomLineParams.bottomMargin = DingDisplayUtil.dp2px(tabBottomHeight - bottomLineHeight, getResources());
+        addView(bottomLine, bottomLineParams);
+        bottomLine.setAlpha(bottomAlpha);
+    }
+
+    private void onSelected(@NonNull DingTabBottomInfo<?> nextInfo) {
+        for (OnTabSelectedListener<DingTabBottomInfo<?>> listener : tabSelectedChangeListeners) {
+            listener.onTabSelectedChange(infoList.indexOf(nextInfo), selectedInfo, nextInfo);
+        }
+        this.selectedInfo = nextInfo;
     }
 
     private void addBackground() {
-
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.ding_bottom_layout_bg, null);
+        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, DingDisplayUtil.dp2px(tabBottomHeight, getResources()));
+        params.gravity = Gravity.BOTTOM;
+        addView(view, params);
+        view.setAlpha(bottomAlpha);
     }
 }

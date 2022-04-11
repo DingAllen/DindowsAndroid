d7fa93deb9ddcd1dde09e3513d08b8c5281fa523
DingAllen <1121174884@qq.com>
2022-04-11 09:50:39 +0800

    下拉刷新的功能完成到处理手势，剩下的之后完成

--- dingui/src/main/java/online/dingod/dingui/refresh/DingRefreshLayout.java
+++ dingui/src/main/java/online/dingod/dingui/refresh/DingRefreshLayout.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.FrameLayout;
+import android.widget.Scroller;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +20,7 @@ public class DingRefreshLayout extends FrameLayout implements DingRefresh {
     protected DingOverView mDingOverView;
     private int mLastY;
     private boolean disableRefreshScroll;
+    private AutoScroller mAutoScroller;
 
     public DingRefreshLayout(@NonNull Context context) {
         super(context);
@@ -35,6 +39,7 @@ public class DingRefreshLayout extends FrameLayout implements DingRefresh {
 
     private void init() {
         mGestureDetector = new GestureDetector(getContext(), dingGestureDetector);
+        mAutoScroller = new AutoScroller();
     }
 
     @Override
@@ -59,8 +64,85 @@ public class DingRefreshLayout extends FrameLayout implements DingRefresh {
 
     DingGestureDetector dingGestureDetector = new DingGestureDetector() {
         @Override
-        public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-            return super.onScroll(motionEvent, motionEvent1, v, v1);
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return super.onScroll(e1, e2, distanceX, distanceY);
         }
     };
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        View head = getChildAt(0);
+        if (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_CANCEL || ev.getAction() == MotionEvent.ACTION_POINTER_INDEX_MASK) {
+            // 松开手
+            if (head.getBottom() > 0) {
+                if (mState != DingOverView.DingRefreshState.STATE_REFRESH) {
+                    recover(head.getBottom());
+                    return false;
+                }
+            }
+            mLastY = 0;
+        }
+        boolean consumed = mGestureDetector.onTouchEvent(ev);
+        if ((consumed || (mState != DingOverView.DingRefreshState.STATE_INIT && mState != DingOverView.DingRefreshState.STATE_REFRESH)) && head.getBottom() != 0) {
+            ev.setAction(MotionEvent.ACTION_CANCEL);
+            return super.dispatchTouchEvent(ev);
+        }
+        if (consumed) {
+            return true;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void recover(int distance) {
+        if (mDingRefreshListener != null && distance > mDingOverView.mPullRefreshHeight) {
+            mAutoScroller.recover(distance - mDingOverView.mPullRefreshHeight);
+            mState = DingOverView.DingRefreshState.STATE_OVER_RELEASE;
+        } else {
+            mAutoScroller.recover(distance);
+        }
+    }
+
+    /**
+     * 借助Scroller实现视图的自动滚动
+     */
+    private class AutoScroller implements Runnable {
+
+        private Scroller mScroller;
+        private int mLastY;
+        private boolean mIsFinished;
+
+        public AutoScroller() {
+            mScroller = new Scroller(getContext(), new LinearInterpolator());
+            mIsFinished = true;
+        }
+
+        @Override
+        public void run() {
+            if (mScroller.computeScrollOffset()) { // 还未滚动完成
+                // TODO: moveDown
+                mLastY = mScroller.getCurrY();
+                post(this);
+            } else {
+                removeCallbacks(this);
+                mIsFinished = true;
+            }
+        }
+
+        void recover(int distance) {
+            if (distance < 0) {
+                return;
+            }
+            removeCallbacks(this);
+            mLastY = 0;
+            mIsFinished = false;
+            mScroller.startScroll(0, 0, 0, distance, 300);
+            post(this);
+        }
+
+        boolean isFinished() {
+            return mIsFinished;
+        }
+    }
+
 }
